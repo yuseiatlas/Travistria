379ec8ef7a0744f1b1eb7285ac46ad79ad91e069
yuseiatlas <rasengan40@gmail.com>
2018-07-17 15:13:37 +0300

    added build flavors

--- app/src/main/java/com/example/yuseiatlas/travistria/MainActivity.java
+++ app/src/main/java/com/example/yuseiatlas/travistria/MainActivity.java
@@ -2,12 +2,14 @@ package com.example.yuseiatlas.travistria;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.v(TAG, "server url is: "+BuildConfig.SERVER_URL);
     }
 }
